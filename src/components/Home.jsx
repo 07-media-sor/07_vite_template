ca8390dd6463a5ae8582d3831a25b06e72fefcdc
Lasse Dollis Spilling <lassespilling@gmail.com>
2022-03-23 11:15:02 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import p from '@/package.json';
+import p from '../../package.json';
 
 const Home = () => {
 	return <h1>{<pre>{JSON.stringify(p, null, 1)}</pre>}</h1>;
